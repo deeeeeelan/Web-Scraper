e193e927cf15e58dc7bf24e0503c7fa828a6e0a4
deeeeeelan <landroid97@gmail.com>
2020-06-06 00:20:52 +0800

    Slight Changes on Endpoints

--- app.py
+++ app.py
@@ -5,16 +5,23 @@ import requests
 import functions
 import json
 
-@app.route("/", methods =['POST'])
+@app.route("/run", methods =['POST'])
 def postmethod():
     link = functions.getLinks()
     data = functions.getData(link)
     res = json.dumps(data)
     return res
 
-@app.route("/", methods =['GET'])
+@app.route("/run", methods =['GET'])
 def getmethod():
     return "Please use POST method instead"
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route("/", methods =['GET','POST'])
+def getmethod():
+    return "Please use /run on POST method"
 
 if __name__ == '__main__':
     app.run()
